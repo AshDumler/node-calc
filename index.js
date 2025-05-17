b47315c02e06fa9c485fb24e22f0b852be5d4b4c
AshDumler <dumlerashleigh@gmail.com>
2025-05-16 20:36:22 -0600

    Did the changes

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const rs = require('readline-sync');
 
 function division() {
-   let num1 = rs.questionInt('Please enter the first number: ');
-   let num2 = rs.questionInt('Please enter the second number: ');
    console.log( 'X = ' + num1 / num2);
 }
 
 function multiplication() {
-   let num1 = rs.questionInt('Please enter the first number: ');
-   let num2 = rs.questionInt('Please enter the second number: ');
    console.log( 'X = ' + num1 * num2);
 }
 
 function subtraction() {
-   let num1 = rs.questionInt('Please enter the first number: ');
-   let num2 = rs.questionInt('Please enter the second number: ');
    let sub = num1 - num2;
    console.log( 'X = ' + sub);
 }
 
 function addition() {
-   let num1 = rs.questionInt('Please enter the first number: ');
-   let num2 = rs.questionInt('Please enter the second number: ');
    let sum = num1 + num2;
    console.log( 'X = ' + sum);
 }
 
-const userCase =  rs.question('What operation would you like to perform? / divide, * multiply, - subtract, + add?: ');
-
-if (userCase == '/') { division(); } 
-else if (userCase == '*') { multiplication(); } 
-else if (userCase == '-') { subtraction(); } 
-else if (userCase == '+') { addition(); } 
-else {
-   console.log('That is not a valid operation');
-   userCase;
+function getNumbers(numPrompt) {
+   const num = rs. questionInt('Please enter the ${numPrompt} number: ');
+   return num;
 }
 
-// The reason I used 'a' instead of '+' is because my terminal does something weird when I try to use '+'
-// Also I know that questionInt makes you have to put in a number in order proceed.
+function main() {
+   const userCase =  rs.question('What operation would you like to perform? / divide, * multiply, - subtract, + add?: ');
+   const num1 = getNumbers('first');
+   const num2 = getNumbers('second');
+
+   if (userCase == '/') { division(num1, num2); } 
+   else if (userCase == '*') { multiplication(num1, num2); } 
+   else if (userCase == '-') { subtraction(num1, num2); } 
+   else if (userCase == '+') { addition(num1, num2); } 
+   else {
+      console.log('That is not a valid operation');
+      userCase;
+   }
+}
+
+main();
